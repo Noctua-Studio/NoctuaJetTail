669aab0ceba730ac930f1c4621243594245c9cac
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2022-09-26 17:32:38 -0500

    actualización botones

--- resources/views/components/web/elements/button.blade.php
+++ resources/views/components/web/elements/button.blade.php
@@ -1,11 +1,15 @@
+<!-- Notas
+    - 
+
+    -->
+
 @props([
     'route' => '#',
     'color' => 'primary',
     'textColor' => 'white',
     'width' => '44',
     'hoverScale' => 'true',
-    'hoverColor' => '',
-    'hoverTextColor' => ''
+    'hoverDarken' => '',
 ])
 
-<a class="nta-btn-envieronmental bg-{{$color}} bg- border-none transition-all duration-100 ease-linear w-{{$width}} text-center py-4 px-7 rounded-md select-none text-{{$textColor}} font-mont font-bold text-xl sm:w-full {{$hoverScale == 'true' ? 'hover:scale-110' : ''}} @if ($hoverColor != '') hover:bg-{{$hoverColor}} @endif @if ($hoverTextColor != '') hover:text-{{$hoverTextColor}} @endif" href="{{$route}}">{{$slot}}</a>
+<a style="background-color: {{$color}}; color: {{$textColor}}" class="nta-btn-envieronmental bg- border-none transition-all duration-100 ease-linear w-{{$width}} text-center py-4 px-7 rounded-md select-none font-mont font-bold text-xl sm:w-full  {{$hoverScale == 'true' ? 'hover:scale-110' : ''}} @if ($hoverDarken == 'true') hover:brightness-90 @endif" href="{{route($route)}}">{{$slot}}</a>
--- resources/views/components/web/hero-w-btn.blade.php
+++ resources/views/components/web/hero-w-btn.blade.php
@@ -6,8 +6,8 @@
         <p class="text-2xl font-normal text-stone-900 phone:text-xl min-w-[100px] max-w-[500px] md:max-w-[400px] mini:max-w-[300px] text-center mx-auto">Subtítulo con texto que explique un poco más de tu negocio</p>
         <div class="home__buttons w-full flex items-center justify-center my-5 mx-auto sm:flex-col gap-6">
 
-            <x-web.elements.button route="" color="environmental" textColor="white" hoverScale="true" hoverColor="white" hoverTextColor="black">Boton 1</x-web.elements.button>
-            <x-web.elements.button route="" color="complementary" textColor="white" hoverScale="true" hoverColor="black">Boton 2</x-web.elements.button>
+            <x-web.elements.button route="products" color="blue" textColor="white" hoverScale="false" hoverDarken="true">Boton 1</x-web.elements.button>
+            <x-web.elements.button route="us" color="black" textColor="white" hoverScale="true" hoverDarken="true">Boton 2</x-web.elements.button>
         </div>
     </div>
 </section>
