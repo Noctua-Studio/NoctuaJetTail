89933be996ecd0e3ace94b5740eeab8bd94611f7
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2022-09-29 21:32:50 -0500

    Estructura menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+        <link rel="shortcut icon" href="{{asset('img/ico/logo-min.ico')}}">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
@@ -21,6 +27,75 @@
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
+
+            <div class="sidebar">
+                <div class="logo__content">
+                    <div class="logo">
+                        <div class="logo__name">Noctua Admin</div>
+                    </div>
+                    <i class="bi bi-list"></i>
+                </div>
+
+                <ul class="nav__list">
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-search"></i>
+                            <input type="text" placeholder="Buscar">
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-house-door-fill"></i>
+                            <span class="links__name">Home</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-laptop-fill"></i>
+                            <span class="links__name">Sitio</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-bag-fill"></i>
+                            <span class="links__name">Productos</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-gear-fill"></i>
+                            <span class="links__name">Configuración</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="bi bi-person-fill"></i>
+                            <span class="links__name">Usuarios</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="profile__content">
+                    <div class="profile">
+                        <div class="profile__details">
+                            {{-- <img src="{{asset('img/placeholder.png')}}" alt=""> --}}
+
+                            <div class="name__job">
+                                <div class="name">Antonio Galván</div>
+                                <div class="job">Administrador</div>
+                            </div>
+                        </div>
+
+                        <i class="bi bi-box-arrow-right" id="log_out"></i>
+                    </div>
+                </div>
+            </div>
+
 
             <!-- Page Heading -->
             @if (isset($header))
