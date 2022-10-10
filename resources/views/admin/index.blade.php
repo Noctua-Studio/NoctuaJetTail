cd43e4f8409e3a9a2b5a749a090ee4b937e6e0d8
AntonioGalvan <62713228+AntonioGalvan@users.noreply.github.com>
2022-10-09 21:22:41 -0500

    y no hay duda alguna xd

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,7 +10,7 @@
                 <img class="w-72" src="{{asset('img/welcome.svg')}}" alt="">
             </div>
             <div class="gap-5 flex-wrap grid grid-cols-5 desktop:grid-cols-3 laptop:grid-cols-2 tablet:grid-cols-1">
-                <x-admin.itemChart bgColor="bg-slate-500"></x-admin.itemChart>
+                <x-admin.itemChart bgColor="bg-"></x-admin.itemChart>
 
                 
             </div>
